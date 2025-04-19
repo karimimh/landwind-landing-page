4dde7fa42f55d7953a2bcf75077f4de39e419555
mhkarimi <bmhkarimi@gmail.com>
2025-04-19 11:43:50 +0330

    #1 create vite/react/typescript/tailwind project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
